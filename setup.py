00486d53073a13402ae4cdb18b65e71b95ab217f
INTP LLC <info@intp.io>
2022-08-26 21:55:44 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from html.entities import name2codepoint
+from setuptools import setup, find_packages
+
+
+setup (
+    name='naics',
+    version='1.0',
+    license='MIT',
+    author='Gray Alien Ventures',
+    author_email='info@intp.io',
+    
+)
