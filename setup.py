e42d2cd6de5e0a4cefe1ad42b6045e8bbf6bb3fd
Khalidalansi <Khalidalansi59@gmail.com>
2022-08-28 22:53:33 +0300

    fix import module

--- setup.py
+++ setup.py
@@ -5,8 +5,17 @@ from setuptools import setup, find_packages
 setup (
     name='naics',
     version='1.0',
+    # url='https://github.com/naics.git',
     license='MIT',
     author='Gray Alien Ventures',
     author_email='info@intp.io',
     packages=find_packages(),
+    # classifiers=[
+    #     "Programming Language :: Python :: 3",
+    #     "License :: OSI Approved :: MIT License",
+    #     "Operating System :: OS Independent",
+    # ],                                      # Information to filter the project on PyPi website
+    py_modules=["naics"],             # Name of the python package
+    package_dir={'':'naics/src'},     # Directory of the source code of the package
+    install_requires=[]                     # Install other dependencies if any
 )
